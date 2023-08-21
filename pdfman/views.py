c1927089e3ed68350f6244a9b18faa1827fc972c
Baraa1 <baraa.amjad1@gmail.com>
2023-08-21 14:58:02 +0300

    Create a file upload UI

--- pdfman/views.py
+++ pdfman/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from pathlib import Path
 
-# Create your views here.
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+OUTPUT_PATH = f'{Path(__file__).resolve().parent.parent}/media/pdf/'
+
+def pdfman(request):
+    return render(request, 'pdfman/pdf.html')
--- tools/urls.py
+++ tools/urls.py
@@ -3,12 +3,12 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import index
-#from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('YouTube/', include('youtube.urls')),
+    path('PDF/', include('pdfman.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
